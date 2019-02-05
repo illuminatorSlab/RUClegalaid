b61fa9b4bb39b75ced94b48f49a67f5a9fe4c3ab
illuminatorStudio <littlel.fox@qq.com>
2019-02-05 19:46:47 +0800

    REFACTOR marriage laws files

---
+++ utils/laws/marriage/index.js
@@ -0,0 +1,18 @@
+const MarriageLaw = require('./marriage-law.js');
+const SuccessionLaw = require('./succession-law.js');
+const MarriageLawInterpretationI = require('./marriage-law-interpretation-i.js');
+const MarriageLawInterpretationII2017Amendment = require('./marriage-law-interpretation-ii-2017-amendment.js');
+const MarriageLawInterpretationII2017SupplementaryProvisions = require('./marriage-law-interpretation-ii-2017-supplementary-provisions.js');
+const MarriageLawInterpretationIII = require('./marriage-law-interpretation-iii.js');
+const AntiDomesticViolenceLaw = require('./anti-domestic-violence-law.js');
+
+module.exports = {
+    "title": "婚姻继承",
+    MarriageLaw,
+    SuccessionLaw,
+    MarriageLawInterpretationI,
+    MarriageLawInterpretationII2017Amendment,
+    MarriageLawInterpretationII2017SupplementaryProvisions,
+    MarriageLawInterpretationIII,
+    AntiDomesticViolenceLaw
+};
