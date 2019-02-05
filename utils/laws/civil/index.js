9107a6e0520cd96447784a616b36670312b14547
illuminatorStudio <littlel.fox@qq.com>
2019-02-05 18:51:07 +0800

    REFACTOR Civil laws files

---
+++ utils/laws/civil/index.js
@@ -0,0 +1,22 @@
+const CivilLawProvisions = require('./civil-law-provisions.js');
+const CivilLawPrinciples = require('./civil-law-principles.js');
+const ContractLaw = require('./contract-law.js');
+const ContractLawInterpretationI = require('./contract-law-interpretation-i.js');
+const ContractLawInterpretationII = require('./contract-law-interpretation-ii.js');
+const TortLaw = require('./tort-law.js');
+const PropertyLaw = require('./property-law.js');
+const GuaranteeLaw = require('./guarantee-law.js');
+const ProvisionsPrivateLendingCases = require('./provisions-private-lending-cases.js');
+
+module.exports = {
+    "title": "民生民权",
+    CivilLawProvisions,
+    CivilLawPrinciples,
+    ContractLaw,
+    ContractLawInterpretationI,
+    ContractLawInterpretationII,
+    TortLaw,
+    PropertyLaw,
+    GuaranteeLaw,
+    ProvisionsPrivateLendingCases
+}
