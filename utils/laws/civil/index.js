f46a623dca3b93b9eb618eceff0e212bf7a4aaee
illuminatorStudio <littlel.fox@qq.com>
2019-02-06 11:24:17 +0800

    FIX 遗漏物权法解释一

--- utils/laws/civil/index.js
+++ utils/laws/civil/index.js
@@ -5,6 +5,7 @@ const ContractLawInterpretationI = require('./contract-law-interpretation-i.js')
 const ContractLawInterpretationII = require('./contract-law-interpretation-ii.js');
 const TortLaw = require('./tort-law.js');
 const PropertyLaw = require('./property-law.js');
+const PropertyLawInterpretationI = require('./property-law-interpretation-i.js');
 const GuaranteeLaw = require('./guarantee-law.js');
 const ProvisionsPrivateLendingCases = require('./provisions-private-lending-cases.js');
 
@@ -17,6 +18,7 @@ module.exports = {
     ContractLawInterpretationII,
     TortLaw,
     PropertyLaw,
+    PropertyLawInterpretationI,
     GuaranteeLaw,
     ProvisionsPrivateLendingCases
 }
