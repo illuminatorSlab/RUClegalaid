aa14dc15a67edda472c77ce035f86ed58af5501c
illuminatorStudio <littlel.fox@qq.com>
2019-02-06 10:43:45 +0800

    REFACTOR procedure laws files

---
+++ utils/laws/procedure/index.js
@@ -0,0 +1,12 @@
+const CivilProcedureLaw2017Revision = require('./civil-procedure-law-2017-revision.js');
+const CivilProcedureLawInterpretation = require('./civil-procedure-law-interpretation.js');
+const CivilProcedureLawInterpretationTrialSupervisionProcedure = require('./civil-procedure-law-interpretation-trial-supervision-procedure.js');
+const AdministrativeLitigationLaw = require('./administrative-litigation-law.js');
+
+module.exports = {
+    "title": "诉讼程序",
+    CivilProcedureLaw2017Revision,
+    CivilProcedureLawInterpretation,
+    CivilProcedureLawInterpretationTrialSupervisionProcedure,
+    AdministrativeLitigationLaw
+};
